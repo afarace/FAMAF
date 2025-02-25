7f12437a93ce61e12ec2a7b1a27549051d54f096
Agustin Farace <agustin.farace@mi.unc.edu.ar>
2025-02-25 18:27:56 -0300

    Agrego lab 1

---
+++ AYED2/lab01/ej4/array_helpers.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "array_helpers.h"
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath)
+{
+
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: file %s not found\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int length = 0;
+    fscanf(file, "%u", &length);
+
+    if (length > max_size)
+    {
+        fprintf(stderr, "Error: array length %u exceeds max size %u\n",
+                length, max_size);
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int i = 0; i < length; i++)
+    {
+        fscanf(file, "%d", &array[i]);
+    }
+
+    fclose(file);
+
+    return length;
+}
+
+void array_dump(int a[], unsigned int length)
+{
+    for (unsigned int i = 0; i < length; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+bool array_is_sorted(int a[], unsigned int length)
+{
+    bool result = true;
+    for (unsigned int i = 0; i < length - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}

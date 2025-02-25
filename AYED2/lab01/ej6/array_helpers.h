f9e88247d873402b739bf8074c1f5c2b5758a8a5
Agustin Farace <agustin.farace@mi.unc.edu.ar>
2025-02-25 18:27:09 -0300

    Agrego lab 1

---
+++ AYED2/lab01/ej6/array_helpers.h
@@ -0,0 +1,16 @@
+#ifndef ARRAY_HELPERS_H
+#define ARRAY_HELPERS_H
+
+#include <stdbool.h>
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath);
+
+void array_dump(int a[], unsigned int length);
+
+void array_swap(int a[], unsigned int i, unsigned int j);
+
+bool array_is_sorted(int a[], unsigned int length);
+
+#endif
